af4cff1531dbe173416a2e61f3f92ee5230b2b94
Vinaysoni2310 <vinaysoni2310@gmail.com>
2023-09-03 08:02:49 +0530

    Final

--- app.py
+++ app.py
@@ -18,69 +18,68 @@ def img2text(filename):
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()[0]['generated_text']
 
-print(img2text('pxfuel.jpg'))
 
 # Text to Story
 
-# def generate_story(scenario):
-#     template = """
-#     You are a story teller;
-#     You can generate a short story on a simple narrative, the story should be no more than 50 words;
+def generate_story(scenario):
+    template = """
+    You are a story teller;
+    You can generate a short story on a simple narrative, the story should be no more than 50 words;
 
-#     CONTEXT = {scenario}
-#     STORY :
-#     """
-#     prompt = PromptTemplate(template=template, input_variables=['scenario'])
+    CONTEXT = {scenario}
+    STORY :
+    """
+    prompt = PromptTemplate(template=template, input_variables=['scenario'])
 
-#     story_llm = LLMChain(llm=OpenAI(
-#         model_name="gpt-3.5-turbo", temperature=1),prompt=prompt,verbose=True
-#     )
+    story_llm = LLMChain(llm=OpenAI(
+        model_name="gpt-3.5-turbo", temperature=1),prompt=prompt,verbose=True
+    )
 
-#     story = story_llm.predict(scenario=scenario)
-#     #print(story)
+    story = story_llm.predict(scenario=scenario)
+    #print(story)
 
-#     return story
+    return story
 
 
 # # Text to Audio
-# def text2speech(story):
-#     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-#     headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
+def text2speech(story):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
-#     payloads ={
-#             'inputs' : story
-#     }
+    payloads ={
+            'inputs' : story
+    }
 
-#     response = requests.post(API_URL, headers=headers, json=payloads)
+    response = requests.post(API_URL, headers=headers, json=payloads)
 
-#     with open('audio.flac', "wb") as f:
-#         f.write(response.content)
+    with open('audio.flac', "wb") as f:
+        f.write(response.content)
 
 
 
-# def main():
-#     st.set_page_config(page_title="Image to Auido Story", page_icon="V")
+def main():
+    st.set_page_config(page_title="Image to Auido Story", page_icon="V")
     
-#     st.header("Turn Image into Audio Story")
-#     uploaded_file = st.file_uploader("Choose an Image...", type="jpg")
+    st.header("Turn Image into Audio Story")
+    uploaded_file = st.file_uploader("Choose an Image...", type="jpg")
 
-#     if uploaded_file is not None:
-#         print(uploaded_file)
-#         bytes_data = uploaded_file.getvalue()
-#         with open(uploaded_file.name, "wb") as file:
-#             file.write(bytes_data)
-#         st.image(uploaded_file, caption = "Uploaded Image.", use_column_width = True)
-#         scenario = img2text(uploaded_file.name)
-#         story = generate_story(scenario)
-#         text2speech(story)
+    if uploaded_file is not None:
+        print(uploaded_file)
+        bytes_data = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+        st.image(uploaded_file, caption = "Uploaded Image.", use_column_width = True)
+        scenario = img2text(uploaded_file.name)
+        story = generate_story(scenario)
+        text2speech(story)
 
-#         with st.expander("scennario"):
-#             st.write(scenario)
-#         with st.expander("story"):
-#             st.write(story)
+        with st.expander("scennario"):
+            st.write(scenario)
+        with st.expander("story"):
+            st.write(story)
         
-#         st.audio("audio.flac")
+        st.audio("audio.flac")
     
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
